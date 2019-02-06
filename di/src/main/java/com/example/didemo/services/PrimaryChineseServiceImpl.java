6fd737c573cf6129b3e8ebda6b33a67078da2244
zliu48 <zliu48@ncsu.edu>
2019-02-06 01:39:09 -0500

    Profile annotation

--- di/src/main/java/com/example/didemo/services/PrimaryChineseServiceImpl.java
+++ di/src/main/java/com/example/didemo/services/PrimaryChineseServiceImpl.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile("en")
-public class PrimaryGreetingImpl implements GreetingService {
+@Profile("cn")
+public class PrimaryChineseServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "use Primary annotation";
+        return "你好 in Chinese";
     }
 }
